21583d7bb562bcaa8099d0a4386c0f3f8887d4e5
MaryyBo <marusya.bodrug@gmail.com>
2024-02-09 13:55:37 +0100

    Practice Task з кнопкою

--- app.js
+++ app.js
@@ -1,52 +1,16 @@
-// console.log(this);
 
-// alert('test'); // Це браузерна річь яка живе в об'єкті Window
-// або window.alert() - одне й те саме
-// document (властивість) - об'єктне уявлення вього нашого документу у якому ми зараз працюємо
+/*  ЗАДАЧА
 
-// console.dir(document); // об'єктне представлення сторінки, все що є на нашй сторінці буде в нашому документі document
-
-/* У DOCUMENT є методи:
- .getElementById (id)
- .getElementByClassName ()
- .getElementByTagName (name)
+    1.зробити кнопку, додати їй ID 
+    2.Напишіть функцію , яка виводить в консоль "привіт"
+    3.Навісити обробку події наведення миші на цю кнопку
 
 */
 
-/*
-    З точки зору браузера всі елементи на сторінці це окремі об'єкти
-Всі елементи можна підписати на події
-Подія, це коли в нас на сторінці щось стається. Банальне наведення курсору, це подія це називається EventTarget
-
-    EventTarget - великий об'єкт що містить купу інформації, куди, на якому елементі сталася подія
-
-    EventTarget має 3 МЕТОДИ:
-    1. addEventListener - дозволяє додати обробник подій , функцію, до певного елемента
-    Приймає 2 параметри  .addEventListener(type, listener)
-
-
-    На MDN є справочник по подіям!!!
-
-*/
-
-// Треба пов'язати кнопку з функцією! Треба звернутись до EventTarget 
-
-function getAlert (){
-    alert('Hello user');
+function consoleHello () {
+    console.log('привіт');
 }
 
-const buttonElement = document.getElementById('btn');
-// console.log(buttonElement);
+const newBtn = document.getElementById('button'); //у змінній ми зберігаємо посилання на нашу кнопку
 
-buttonElement.addEventListener('click',getAlert);
-
-//1. зареєстрували обробник події натискання миші на кнопку
-//2. навісили обробник натискання миші на кнопку
-
-/*  
-    1. дати клас, id, тег елементу.(в html)
-    2. прописати функцію (що буде виконуватись при клацанні мишкою на кнопку)
-    3. cтворити змінну для кнопки(елементу) в якій прописати потрібний метод Document (document.getElementById('btn'))
-    4. до створеної змінної buttonElement приміняємо метод .addEventListener(type, listener)
-
-*/
+newBtn.addEventListener('mouseover',consoleHello);
