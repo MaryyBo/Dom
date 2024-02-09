06f45e75c8e4c468b51b00b5c344125e84b88b93
MaryyBo <marusya.bodrug@gmail.com>
2024-02-09 16:17:53 +0100

    .removeEventListener

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-function getAlert (event){
-    // console.log(event);
-    const btn = event.target; // ми заходимо в об'єкт event і дістаємо target
+/* ЗАДАЧА
+    При навееденні на кнопку "Ні" потрібно її приховати (display: none)
+*/
+
+
+// 1. Cпочатку кнопку треба знайти вводимо document і створюємо змінну
+const noBtn = document.getElementById('no-btn');
+
+// const noBtn = $('#no-btn')
+
+// 2. Зробити обробник події
+
+function hideBtn (event) { // в event буде target
    
+    console.log(event.target);
+    const btn = event.target; // кнопка на якій спрацювала подія
+    btn.style.display = 'none';
+    // console.log('test');
 
-    if (btn.style.backgroundColor === 'red') {
-        btn.style.backgroundColor = 'unset';
-    } else {
-        btn.style.backgroundColor = 'red'
-    }
-}
+    // event.target.removeEventListener('mouseenter', hideBtn);
+    
+} //  Якщо потрібно , щоб кнопка була одноразовою
 
-const buttonElement = document.getElementById('btn');
-// console.log(buttonElement);
+noBtn.addEventListener('mouseenter', hideBtn);
 
-buttonElement.addEventListener('click',getAlert);
+/* 
+
+За правилом хорошого тону, при скриванні кнопку як в даному випадку, 
+краще так не робити, тому що addEventListener всеодно існує, і нам
+краще видалити цей addEventListener саме це і робить метод 
+
+- .removeEventListener (type, listener)
+
+*/
+
