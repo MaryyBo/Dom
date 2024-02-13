f9634d5679a5d6dcd8c8b0ef4aef0254ae04ccea
MaryyBo <marusya.bodrug@gmail.com>
2024-02-13 18:07:48 +0100

    Disable атрибут

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
 
-
-
 const button = document.querySelector('button');
-const div = document.querySelector('#root');
 
+button.addEventListener('click',clickHandler );
 
-
-function eventHandlerButton (event) {
-    console.log('Hi from button handler');
-    
+function clickHandler ({target}) {
+    target.disabled = true; // відключить ПОВНІСТЮ кнопку після 1 натиску
 }
 
-const eventHandlerBody = (event) => {
-    console.log('Hi from body handler');
-    event.stopPropagation();             //  event.stopPropagation();  далі нічого не піде, кнопка зупиниться на body
-    // console.dir(event.currentTarget);
-    console.log(this);
-    
-    // в середині Function Declaration та Function Expression - this буде --> body
-    // коли ж ми кажемо за Arrow Function, то в неї немає this, і this буде --> Window
-}
-
-button.addEventListener('click', eventHandlerButton, {capture: true});
-// div.addEventListener('click', eventHandler, {capture: true});
-document.body.addEventListener('click', eventHandlerBody, {capture: true});
-// window.addEventListener('click', eventHandler,{capture: true});
-
+// target - ми витягли {} з event деструктурували, працює так само, як би ми там записали event
