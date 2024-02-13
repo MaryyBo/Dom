e40442f0f2c953d6b639e13a85670ee71b2fa94e
MaryyBo <marusya.bodrug@gmail.com>
2024-02-13 18:41:04 +0100

    ParentNode + dataset

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+// date-* атрибути наприклад <button data-color="red">Red</button>
 
-const button = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 
-button.addEventListener('click',clickHandler );
+const div = document.querySelector('#root');
 
-function clickHandler ({target}) {
-    target.disabled = true; // відключить ПОВНІСТЮ кнопку після 1 натиску
+console.dir(buttons);
+
+// for(let i=0; i< buttons.length; i++) {
+//     buttons[i].addEventListener('click', clickHandler);
+// }
+
+for (let btn of buttons) {
+    btn.addEventListener('click',clickHandler)
 }
 
-// target - ми витягли {} з event деструктурували, працює так само, як би ми там записали event
+function clickHandler ({target: {dataset: {color}, parentNode}}) {
+    // const {dataset: {color}} = target;
+    // const {parentNode} = target;
+
+    parentNode.style.backgroundColor = color;
+    
+    // div.style.backgroundColor = color;
+}
