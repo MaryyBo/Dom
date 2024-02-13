fa55cb25188b19f1c4b7c74100093dd731240795
MaryyBo <marusya.bodrug@gmail.com>
2024-02-13 19:25:36 +0100

    img src task - задача

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-// date-* атрибути наприклад <button data-color="red">Red</button>
+//https://png.pngtree.com/thumb_back/fw800/background/20230610/pngtree-picture-of-a-blue-bird-on-a-black-background-image_2937385.jpg
+//https://img.freepik.com/free-photo/bright-petals-gift-love-in-a-bouquet-generated-by-ai_188544-13370.jpg?size=626&ext=jpg&ga=GA1.1.1141335507.1707696000&semt=ais
 
-const buttons = document.querySelectorAll('button');
 
-const div = document.querySelector('#root');
+const [btn1, btn2] = document.querySelectorAll('button');
 
-console.dir(buttons);
+btn1.addEventListener('click',clickHandler);
+btn2.addEventListener('click',clickHandler);
 
-// for(let i=0; i< buttons.length; i++) {
-//     buttons[i].addEventListener('click', clickHandler);
-// }
 
-for (let btn of buttons) {
-    btn.addEventListener('click',clickHandler)
+function clickHandler ({target}) {
+ const img = target.parentNode.children[0];
+ img.setAttribute ('src', target.dataset.src);
+
+
 }
 
-function clickHandler ({target: {dataset: {color}, parentNode}}) {
-    // const {dataset: {color}} = target;
-    // const {parentNode} = target;
+//Метод  setAttribute передаємо 2 значення (name, value)
+// Треба достукатись до адреси картинки
 
-    parentNode.style.backgroundColor = color;
-    
-    // div.style.backgroundColor = color;
-}
