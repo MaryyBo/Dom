51e21a3bb604200df573f54e0bbcd189b0b2c5f8
MaryyBo <marusya.bodrug@gmail.com>
2024-02-15 14:24:09 +0100

    HomeTAsk

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
- 
-
-
- function createDiv () {
-    const div = document.createElement('div');// створюємо елемент у пам'яті
-    div.classList.add('box') //  підєднали клас .box зі стилів
-    div.append('Hello world'); // приєднуємо текстовий вузол
-    console.log(div);
-    
-     document.body.append(div); // динамічно додали до HTML
- }
-
- /* 
- <body> 
- <div class ="box"> Helloworld </div>
-  </body> 
- */
-
- const btn = document.querySelector('button');
-
- btn.addEventListener('click',createDiv );
-  
- // element.append 
- // document.createElement
- // classList.add
 
  // ЗАДАЧА додому 
  /*Спробуйте створити декілька HTML елементів через JS. (document.createElement)
@@ -30,3 +5,44 @@
 Спробуйте додати якийсь текстовий вузол для елементів. (  element.append('text....')  )
 Причепіть створені елементи до body (  document.body.append(element)  )
 */
+//https://www.poznavayka.org/wp-content/uploads/2017/07/Krasnyiy-volk.jpg
+
+function createDiv () {
+    
+    const divElement = document.createElement('div');
+    divElement.classList.add('div-class');
+
+    const sectionElement = document.createElement('section');
+    sectionElement.classList.add('section-class');
+    divElement.append(sectionElement);
+    
+
+    const H2Element = document.createElement('h2');
+    H2Element.append('Рудий вовк');
+    sectionElement.append(H2Element);
+
+
+    const H3Element = document.createElement('h3');
+    H3Element.append('Найбільш рідкісна тварина у світі, яка незабаром може зникнути');
+    sectionElement.append(H3Element);
+
+    
+
+    const pElement = document.createElement('p')
+    pElement.append('Живе в Східній частині США. Загрозою для розмноження цього виду стали звичайні фермери. Існувало три види цих тварин, але сьогодні, на жаль, залишився тільки один. Цей вид занесений у Червону книгу. ');
+    divElement.classList.add('p');
+    sectionElement.append(pElement);
+
+
+    const imgElement = document.createElement('img')
+    imgElement.src="https://www.poznavayka.org/wp-content/uploads/2017/07/Krasnyiy-volk.jpg";
+
+    sectionElement.append(imgElement);
+
+    console.log(divElement);
+
+    document.body.append(divElement);
+}
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', createDiv);
