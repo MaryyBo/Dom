322e17282bf7a6a6be84d1d86a995a241c445a8b
MaryyBo <marusya.bodrug@gmail.com>
2024-02-19 18:02:38 +0100

    Create ToDo list Oновлення з коментарями

--- app.js
+++ app.js
@@ -24,26 +24,57 @@ buttonToDo.addEventListener('click', addToDoList)
 function addToDoList(event) {
   event.preventDefault();
 
+   // Отримання значення з інпута
   const input = document.querySelector('#text');
-  const text = input.value;
+  const text = input.value.trim(); //value - це те, що вводять в input - текст
+  console.log(input.value);
 
+  // Створення нового елементу списку
   const li = document.createElement('li');
   li.innerHTML = text;
 
   const delButton = document.createElement('button');
   delButton.addEventListener('click', deleteToDoList);
   delButton.innerText = 'Delete';
+  delButton.classList.add('remove-btn');
 
+  // Додавання кнопки видалення до елементу списку
   li.append(delButton);
 
+  // Отримання посилання на список і додавання нового елементу
   const ul = document.querySelector('ul');
   ul.append(li);
 
   input.value = '';// видалити попередньо введений текст з input
+  
+  console.dir(this);
+}
+
+// 1 Variant
+
+// function deleteToDoList() {
+//   // const liToDelete = this.parentElement;
+//   // liToDelete.remove();
+//   this.parentNode.remove();  
+//   console.dir(this); 
+//   // this вказує на кнопку видалення, parentNode вказує на батьківський елемент (<li>)
+// }
+
+// 2 Variant
+
+function deleteToDoList({target: {parentNode}}) {
+  parentNode.remove();
 }
 
 
-function deleteToDoList() {
-  const liToDelete = this.parentElement;
-  liToDelete.remove();
-}
+
+/* 
+Якщо ви викликаєте console.dir(this) всередині функції addToDoList, ви можете побачити в консолі об'єкт,
+ який представляє елемент, на якому відбувається подія, і дізнатися більше про його властивості та методи.
+
+*/
+
+
+
+
+
