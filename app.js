40eb69bcf067e8f7cd478c12dce40a8d252527cf
MaryyBo <marusya.bodrug@gmail.com>
2024-02-23 17:27:32 +0100

    setTimeout in promise

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-// Promise - Це звичайний об'єкт.
+const p = new Promise(executor); // pending 
 
-const p = new Promise(executor); // записали в змінну - р в статусі pending
-
-function executor (resolve, reject) { // пояснюємо коли проміс має перейти в статус resolve або reject
-    // Задача: Згенерувати випадкове ціле число в діапазонні від 0 до 15;
-    const number = Math.floor(Math.random()* 15);
-
-    //Якщо число кратне 2 -> resolve проміс та повертаємо це число
-    //Якщо число НЕ кратне 2 -> reject проміс та повертаємо помилку
-
-    if (number % 2 === 0) {
-        resolve(number); fulfilled
-    } else {
-        const err = new RangeError (`Error happened ${number}`);
-        reject (err);
-    }
-  
+function executor(resolve, reject)  {
+    setTimeout(() => {
+        resolve('It time'); //а через 15сек переходить в статус fulfilled
+    }, 15000)
 }
+const h1 = document.querySelector('#root');
 
-p.then((number) => {
-    console.log(number);
-}, (err) => {
-    console.log(err);
-} ) // 2 колбек : 1 попадає в resolve, 2 попадає в reject
-
+p.then((str) => {
+    h1.append(str);
+});
