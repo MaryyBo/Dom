26c0f259d239ba9ef42660ddcbeb3d9a41faa8c8
MaryyBo <marusya.bodrug@gmail.com>
2024-02-26 12:04:03 +0100

    previousWidgetRemove() - оновлене

--- app.js
+++ app.js
@@ -1,51 +1,86 @@
-function loadImage (src) {
-    // 1. Створюємо саму картинку
-    const img = document.createElement('img');
-    img.setAttribute('src', src);
+/*
 
-    // 2. Створюємо елемент у верстці що вкаже, чи вантажимо ми щось чи ні 
-    const h2 = document.createElement('h2');
-    h2.id = 'loading-h2'   // видаємо id
-    h2.append('Loading image...') // додаємо текстовий вузол
-    document.body.append(h2); // відображаємо поки грузиться картинка смс "Loading image..."
+https://api.openweathermap.org/data/2.5/weather?q=Dnipro&appid=f7c576ba3699bdd0b98ddcf196639992&units=metric
 
-   
+code - f7c576ba3699bdd0b98ddcf196639992
+*/
 
+/*Задача: зробити погодний віджет
 
-    // 3. Повертаємо promise
-    return new Promise ((resolve, reject) => {
-        // 3. Створюємо setTimeout
-      const timeOutId = setTimeout (() => {
-            reject ('Image can`t be loaded')
-        }, 5000)
+ Алгоритм вирішення:
 
-        // Підписуємо свторену картинку на подію load
-        // Якщо картина завантажеться  - ми резолвимо проміс з елементом картинки
-        img.addEventListener('load', () => {
-            clearTimeout(timeOutId);
-            resolve(img);
-        })
-        // Підписуємо свторену картинку на подію error
-        // Якщо картина HE завантажеться  - ми реджектимо з повідомлення про помилку
-        img.addEventListener('error', () => {
-            clearTimeout(timeOutId);
-            reject('Image can`t be loaded');
-        })
-    })
+1. Зробити верстку елементів, які отримують від користувача дані про місто
+2. Отримати дані з APIі обробити іх ( підготувати дані для відмалювання у верстці)
+3. Зробити картку з погодою і відобразити її.
 
+*/
 
+const API_KEY = 'f7c576ba3699bdd0b98ddcf196639992';
+const API_BASE = 'https://api.openweathermap.org/data/2.5/weather';
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', buttonlickHandler);
+
+function buttonlickHandler({ target }) {
+    const selectValue = target.previousElementSibling.value;
+    requestAPI(selectValue);
 }
 
-loadImage('https://masterpiecer-images.s3.yandex.net/5377dfff87a111ee99d3ca123cab517e:upscaled')
-.then ((img) => {
-    document.body.append(img);
+function requestAPI(cityName) {
+    //1. Готуємо URL 
+    const url = `${API_BASE}?q=${cityName}&appid=${API_KEY}&units=metric`
+    console.log(url)
+    //2. Робимо запит 
+    fetch(url)
+        .then((response) => {
+            return response.json() // преобразовуєм 0 та 1 в JS
+        })
+        .then((data) => {
+            //3. Відмальовуємо погоду
 
-}, (errorMessage) => {
-    const h2 = document.createElement('h2');
-    h2.append(errorMessage);
-    document.body.append(h2);
-})
-.finally (() => {
-    const loadingH2 = document.querySelector('#loading-h2')
-    loadingH2.remove();
-})
+            clearPreviousWidget();// Функція видалення попереднього віджета
+
+            displayWeather(data);
+        })
+        
+
+/*<article class="weather">
+<p>City name: Kyiv</p>
+<p>Temperature: 7°C</p>
+<p>Weather description: overcast clouds</p>
+</article>
+*/
+
+function displayWeather(weatherObject) {
+    const { name, main: { temp }, weather: [{ description }] } = weatherObject;
+    //1.Створємо article
+    const article = document.createElement('article')
+    article.classList.add('weather')
+
+    //2.Створємо параграф з назвою міста
+    const cityName = document.createElement('p');
+    cityName.append(`City name:${name}`);
+
+    //3.Створємо параграф з температурою
+    const temperature = document.createElement('p');
+    temperature.append(`Temperature:${temp}°C`);
+
+    //4.Створємо параграф з описом погоди
+    const weatherDescription = document.createElement('p');
+    weatherDescription.append(`Weather description: ${description}`);
+
+    //5. До article чіпляємо параграфи з пунктів 2-4
+    article.append(cityName, temperature, weatherDescription);
+
+    //5. Знаходимо section і чіпляємо до неї article
+    const section = document.querySelector('.wrapper');
+
+    section.append(article);
+}}
+
+
+function previousWidgetRemove() {
+    // Треба знайти раніше створений віджет і видалити його перед тим як створити новий
+    const previousWidget = document.querySelector('.weather');
+    previousWidget.remove();
+}
