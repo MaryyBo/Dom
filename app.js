4abcb3ed337aa128c8ef185825d590ce28fda5fa
MaryyBo <marusya.bodrug@gmail.com>
2024-02-28 15:51:06 +0100

    HomeWork Creating a User Card

--- app.js
+++ app.js
@@ -1,31 +1,85 @@
-/* 
+/*
 
-async
-await     - скорочуємо код з допомогою цих слів
+Задача 1
 
+У вас є сторінка.
+На сторінці є кнопка "підвантажити юзера".
+При натисненні на цю кнопку, на сторінку має додатися картка юзера.
+
+Картка може містити:
+- аватарка
+- ім'я, прізвище
+- імейл
+- адреса
+- день народження
+
+https://randomuser.me/api
 */
 
-const URL = 'https://randomuser.me/api';
+const API_BASE = 'https://randomuser.me/api';
 
-function loadDataWithoutAsync(url) {
-    const p = fetch(url);
-    const jsonResponse = p.then((response) => {
-        return response.json();
+const button = document.querySelector('#addUserCard');
+button.addEventListener('click', apiRequest);
+
+
+function apiRequest() {
+  fetch(API_BASE)
+    .then((response) => {
+      return response.json()
     })
-    jsonResponse.then((data) => {
-        console.log(data);
+    .then((data) => {
+      console.log(data);
+      displayUser(data);
     })
 }
 
-// loadDataWithoutAsync (URL);// виклик звичайної функції
 
-// Створення ASYNC / AWAIT FUNCTION (коротший синтаксис)
+const divWrapper = document.createElement('div'); // загальний <div> до якого потім будуть appendитись sections з картками
+divWrapper.classList.add('div-wrapper');
+document.body.append(divWrapper); // appendимо до BODY і в стилях налаштовуємо так, як хочемо щоб відображались картки(section)
 
-async function loadDataAsync(url) {
-    const p = fetch(url);
-    const response = await p;
-    const data = await response.json();
-    console.log(data);
+
+function displayUser(userObject) {
+
+  const { name, email, location, dob, picture } = userObject.results[0];
+  const fullName = `${name.first} ${name.last}`;
+  // console.log(fullName);
+
+  const userCardContainer = document.createElement('section');
+  userCardContainer.classList.add('user-card');
+
+  const avatar = document.createElement('img');
+  avatar.setAttribute('src', picture.large);
+  avatar.setAttribute('alt', fullName);
+  avatar.classList.add('card-image')
+
+
+  const  userName = document.createElement('p');
+  userName.append(`Name: ${name.first}`);
+
+  const userSurname = document.createElement('p');
+  userSurname.append(`Surname: ${name.last}`);
+
+  const emailElem = document.createElement('p');
+  emailElem.append(`Email: ${email}`);
+
+  const locationElem = document.createElement('p');
+  locationElem.append(`Location: ${location.country}`);
+
+  const birthDate = document.createElement('p');
+  birthDate.append(`Age: ${dob.age}`);
+
+
+
+  userCardContainer.append(avatar, userName, userSurname, birthDate, locationElem, emailElem);
+
+  divWrapper.appendChild(userCardContainer);
+
 }
 
-loadDataAsync(URL);
+
+
+
+
+
+
